4eee66164c7e6c36e36e65ab5b8a1ba5dad09be9
iguer <iguer@b8d9039a-ea7d-4afb-bb93-26d10a78e78a>
2011-03-21 09:15:26 +0000

    branchement des explications dans Arrays en cours
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/alt-ergo/trunk@53 b8d9039a-ea7d-4afb-bb93-26d10a78e78a

--- options.ml
+++ options.ml
@@ -210,5 +210,5 @@ let goal_directed = !goal_directed
 let bouclage = ! bouclage
 let max_split = !max_split
 let rewriting = !rewriting
-let proof = !proof
-let debug_proof = !debug_proof && proof
+let proof = !proof || true
+let debug_proof = !debug_proof && proof || true
