db90320f36127ce8f04fa13fb8c679c8f44ecd2c
mebsout <mebsout@b8d9039a-ea7d-4afb-bb93-26d10a78e78a>
2011-10-18 14:02:46 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/alt-ergo/trunk@210 b8d9039a-ea7d-4afb-bb93-26d10a78e78a

--- tests/qualification/qualif.sh
+++ tests/qualification/qualif.sh
@@ -1,4 +1,5 @@
 ../../util/test -n "../../alt-ergo.opt -parse-only -qualif 0" ../parsing/parsing.split
 ../../util/test -n "../../alt-ergo.opt -type-only -qualif 1" ../typing/typing.split
 ../../util/test -n "../../alt-ergo.opt -qualif 2" ../sat/sat.split
+../../util/test -n "../../alt-ergo.opt -qualif 3" ../cc_uf_ac/cc_uf_ac.split 
  rubber -d tests.tex
