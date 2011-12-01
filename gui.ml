9378eec8f33a118669ab7fa374f5339d8e417eff
conchon <conchon@b8d9039a-ea7d-4afb-bb93-26d10a78e78a>
2011-12-01 21:53:39 +0000

    new header
    git-svn-id: svn://scm.gforge.inria.fr/svn/alt-ergo/trunk@252 b8d9039a-ea7d-4afb-bb93-26d10a78e78a

--- gui.ml
+++ gui.ml
@@ -1,3 +1,22 @@
+(**************************************************************************)
+(*                                                                        *)
+(*     The Alt-ergo theorem prover                                        *)
+(*     Copyright (C) 2006-2011                                            *)
+(*                                                                        *)
+(*     Sylvain Conchon                                                    *)
+(*     Evelyne Contejean                                                  *)
+(*                                                                        *)
+(*     Francois Bobot                                                     *)
+(*     Stephane Lescuyer                                                  *)
+(*     Mohamed Iguernelala                                                *)
+(*     Alain Mebsout                                                      *)
+(*                                                                        *)
+(*     CNRS - INRIA - Universite Paris Sud                                *)
+(*                                                                        *)
+(*   This file is distributed under the terms of the CeCILL-C licence     *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Why_ptree
 open Why_annoted
 open Why_connected
