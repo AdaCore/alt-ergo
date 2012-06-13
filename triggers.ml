709a8a4139d073b8cef46bd072d53ba489430e55
mebsout <mebsout@b8d9039a-ea7d-4afb-bb93-26d10a78e78a>
2012-06-13 18:23:16 +0000

    unique sort in triggers
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/alt-ergo/trunk@463 b8d9039a-ea7d-4afb-bb93-26d10a78e78a

--- triggers.ml
+++ triggers.ml
@@ -220,10 +220,138 @@ let compare_tterm_list tl2 tl1 =
 	 if c <> 0 then raise (Out c)
       ) tl1 tl2; 0 
   with Out c -> c end
-    
+
+
+module Uniq_sort = struct
+  let rec merge cmp l1 l2 =
+    match l1, l2 with
+      | [], l2 -> l2
+      | l1, [] -> l1
+      | h1 :: t1, h2 :: t2 ->
+	let c = cmp h1 h2 in
+	if c = 0 then h1 :: merge cmp t1 t2
+	else if c < 0 then h1 :: merge cmp t1 l2
+	else h2 :: merge cmp l1 t2
+
+
+  let rec chop k l =
+    if k = 0 then l else begin
+      match l with
+	| x::t -> chop (k-1) t
+	| _ -> assert false
+    end
+  ;;
+
+  let stable_sort cmp l =
+    let rec rev_merge l1 l2 accu =
+      match l1, l2 with
+	| [], l2 -> List.rev_append l2 accu
+	| l1, [] -> List.rev_append l1 accu
+	| h1::t1, h2::t2 ->
+          let c = cmp h1 h2 in
+	  if c = 0 then rev_merge t1 t2 (h1::accu)
+	  else if c < 0 then rev_merge t1 l2 (h1::accu)
+          else rev_merge l1 t2 (h2::accu)
+    in
+    let rec rev_merge_rev l1 l2 accu =
+      match l1, l2 with
+	| [], l2 -> List.rev_append l2 accu
+	| l1, [] -> List.rev_append l1 accu
+	| h1::t1, h2::t2 ->
+          let c = cmp h1 h2 in
+	  if c = 0 then rev_merge_rev t1 t2 (h1::accu)
+	  else if c > 0 then rev_merge_rev t1 l2 (h1::accu)
+          else rev_merge_rev l1 t2 (h2::accu)
+    in
+    let rec sort n l =
+      match n, l with
+	| 2, x1 :: x2 :: _ ->
+	  let c = cmp x1 x2 in
+	  if c = 0 then [x1] 
+	  else if c < 0 then [x1; x2] else [x2; x1]
+	| 3, x1 :: x2 :: x3 :: _ ->
+	  let c = cmp x1 x2 in
+	  if c = 0 then begin
+	    let c = cmp x2 x3 in
+	    if c = 0 then [x1]
+            else if c <= 0 then [x1; x3]
+            else [x3; x1]
+	  end
+	  else if c < 0 then begin
+	    let c = cmp x2 x3 in
+	    if c = 0 then [x1; x2]
+            else if c < 0 then [x1; x2; x3]
+            else 
+	      let c = cmp x1 x3 in
+	      if c = 0 then [x1; x2]
+	      else if c < 0 then [x1; x3; x2]
+              else [x3; x1; x2]
+	  end else begin
+	    let c = cmp x1 x3 in
+	    if c = 0 then [x2; x1]
+            else if c < 0 then [x2; x1; x3]
+            else
+	      let c = cmp x2 x3 in
+	      if c = 0 then [x2; x1]
+	      else if c < 0 then [x2; x3; x1]
+              else [x3; x2; x1]
+	  end
+	| n, l ->
+	  let n1 = n asr 1 in
+	  let n2 = n - n1 in
+	  let l2 = chop n1 l in
+	  let s1 = rev_sort n1 l in
+	  let s2 = rev_sort n2 l2 in
+	  rev_merge_rev s1 s2 []
+    and rev_sort n l =
+      match n, l with
+	| 2, x1 :: x2 :: _ ->
+	  let c = cmp x1 x2 in
+	  if c = 0 then [x1] 
+	  else if c > 0 then [x1; x2] else [x2; x1]
+	| 3, x1 :: x2 :: x3 :: _ ->
+	  let c = cmp x1 x2 in
+	  if c = 0 then begin
+	    let c = cmp x2 x3 in
+	    if c = 0 then [x1]
+            else if c > 0 then [x1; x3]
+            else [x3; x1]
+	  end
+	  else if c > 0 then begin
+	    let c = cmp x2 x3 in
+	    if c = 0 then [x1; x2]
+            else if c > 0 then [x1; x2; x3]
+            else
+	      let c = cmp x1 x3 in
+	      if c = 0 then [x1; x2]
+	      else if c > 0 then [x1; x3; x2]
+              else [x3; x1; x2]
+	  end else begin
+	    let c = cmp x1 x3 in
+	    if c = 0 then [x2; x1]
+            else if c > 0 then [x2; x1; x3]
+            else
+	      let c = cmp x2 x3 in
+	      if c = 0 then [x2; x1]
+	      else if c > 0 then [x2; x3; x1]
+              else [x3; x2; x1]
+	  end
+	| n, l ->
+	  let n1 = n asr 1 in
+	  let n2 = n - n1 in
+	  let l2 = chop n1 l in
+	  let s1 = sort n1 l in
+	  let s2 = sort n2 l2 in
+	  rev_merge s1 s2 []
+    in
+    let len = List.length l in
+    if len < 2 then l else sort len l
+end
+
+
 
 let at_most n l =
-  let l = List.fast_sort compare_tterm_list l in
+  let l = Uniq_sort.stable_sort compare_tterm_list l in
   let rec atmost acc n l = 
     match n, l with
       | n, _ when n <= 0 -> acc
