e6799438895447ed18f1f94d922b3ae92bbda5e7
mebsout <mebsout@b8d9039a-ea7d-4afb-bb93-26d10a78e78a>
2012-08-29 16:21:52 +0000

    bug make div and modulo
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/alt-ergo/trunk@539 b8d9039a-ea7d-4afb-bb93-26d10a78e78a

--- arith.ml
+++ arith.ml
@@ -148,7 +148,7 @@ module Make
               if approx then mk_euc_division p p2 t1 t2 ctx
               else p, ctx
 	    with Division_by_zero | Polynome.Maybe_zero -> 
-              P.create [coef, X.term_embed t] (Int 0) ty, ctx
+              P.create [Int 1, X.term_embed t] (Int 0) ty, ctx
 	  in
 	  P.add p (P.mult (P.create [] coef ty) p3), ctx
 		
@@ -172,7 +172,7 @@ module Make
                 | Polynome.Not_a_num -> mk_modulo t t1 t2 p2 ctx
                 | _ -> assert false 
               in 
-              P.create [coef, X.term_embed t] (Int 0) ty, ctx 
+              P.create [Int 1, X.term_embed t] (Int 0) ty, ctx 
 	  in         
 	  P.add p (P.mult (P.create [] coef ty) p3), ctx
 	    
@@ -182,6 +182,7 @@ module Make
 	match C.extract a with
 	  | Some p' -> P.add p (P.mult (P.create [] coef ty) p'), ctx
 	  | _ -> P.add p (P.create [coef, a] (Int 0) ty), ctx
+
 
   let arith_to_ac p = p
 (*
