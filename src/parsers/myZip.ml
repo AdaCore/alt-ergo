b9d9fb5f5932d292df5819abd95ce8bfdec730bb
Johannes Kanig <kanig@adacore.com>
2022-06-13 17:25:19 +0900

    V613-001 remove zip dependency of alt-ergo
    Change-Id: Ie0809559ea1397de4061b908e4b3f2478e11a6c9

--- src/parsers/myZip.ml
+++ src/parsers/myZip.ml
@@ -12,28 +12,29 @@
 (** A wrapper of the Zip module of CamlZip: we use Zip except when we want to
     generate the.js file for try-Alt-Ergo **)
 
+(* Commented out for SPARK FSF 12
+
 module ZipWrapper = struct
   include Zip
   let filename e = e.Zip.filename
   let is_directory e = e.Zip.is_directory
 end
 
-include ZipWrapper
-
-(* !! This commented code is used when compiling to javascript !!
-   module DummyZip = struct
-   type entry = unit
-   type in_file = unit
-
-   let s = "Zip module not available for your setting or has been disabled !"
-
-   let open_in  _  =  failwith s
-   let close_in _ = failwith s
-   let entries  _  =  failwith s
-   let read_entry  _ _  =  failwith s
-   let filename  _  =  failwith s
-   let is_directory  _  =  failwith s
-   end
-
-   include DummyZip
 *)
+
+
+module DummyZip = struct
+type entry = unit
+type in_file = unit
+
+let s = "Zip module not available for your setting or has been disabled !"
+
+let open_in  _  =  failwith s
+let close_in _ = failwith s
+let entries  _  =  failwith s
+let read_entry  _ _  =  failwith s
+let filename  _  =  failwith s
+let is_directory  _  =  failwith s
+end
+
+include DummyZip
